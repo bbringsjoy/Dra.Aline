595f82010fcb80f29ef35e483276cdbd8b22e1e3
Bia - Integrado <biagsantana1000@gmail.com>
2025-05-20 19:55:20 -0300

    commit comecando o php/tentando

---
+++ agendar-horario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Entre em contato</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="imagens/logo2-removebg-preview.png" rel="shortcut icon">
+</head>
+<body>
+    <h2 class="text-center">Agendar horário</h2>
+</body>
+</html>
