b82f476af7c5d9c23217bac838e1d4a79fdfb26a
Bia - Integrado <biagsantana1000@gmail.com>
2025-06-02 20:04:49 -0300

    commit comecando php

--- index.php
+++ index.php
@@ -1,3 +1,206 @@
-<?php
-    $param = $_GET['param'];
-    echo $param;
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/all.min.css"> <!-- Font Awesome -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link href="imagens/logo2-removebg-preview.png" rel="shortcut icon">
+
+    <title> Dr.Aline Gonçalves </title>
+  
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"><!-- animação git -->
+
+
+
+</head>
+<body>
+    <header>
+      
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#home">
+                <img src="imagens/logo2-removebg-preview.png" alt="Logo" class="w-100">
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class= listas>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#home">
+                      <i class="fa-solid fa-house"></i> Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#sobre">
+                      <i class="fa-solid fa-user-doctor"></i> Sobre</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#endereco">
+                      <i class="fa-solid fa-location-dot"></i> Onde?</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="link do php">
+                      <i class="fa-solid fa-calendar-days"></i> Agendar horário</a>
+                  </li>
+                </div>
+            </div>
+          </div>
+          </nav>
+      
+          
+    </header>
+    <main>
+      <div data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+        <section class="container" id="home">
+          <div class="row row-cols-2">
+            <div class="col">
+              <img src="imagens/Aline-1.jpg" alt="Foto Dr.Aline" class="img-fluid">
+            </div>
+            <div class="col">
+              <h1> Aline </h1>
+              <br>
+              <p>
+                A Dra. Aline Gonçalves possui um atendimento humanizado na Odontologia tanto 
+                em crianças quanto adultos.
+              </p>
+              <p>
+                Venha conhecer o trabalho dela!
+              </p>
+              <div class="contatos">
+              <p>
+                <a href="https://www.instagram.com/alinegd28?igsh=MTduOHV2cG1qNXNocg==">
+                  <i class="fa-brands fa-instagram"></i>
+                  </i>
+                </a>
+                <a href="https://www.facebook.com/share/16SygXo4Q2/">
+                  <i class="fa-brands fa-facebook"></i>
+                  </a>
+                  <a href= "https://wa.link/cn062u">
+                    <i class="fa-brands fa-whatsapp"></i>
+                  </a>
+              </p>
+              </div>
+              </div>
+            </div>
+            </div>
+        </section>
+        <div class="whats">
+          <a href= "https://wa.link/cn062u">
+            <i class="fa-brands fa-whatsapp"></i>
+        </a>
+      </div>
+      <div data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+      <section class="sobre" id="sobre">
+        <h3 class="text-center">Sobre</h3>
+        <div class="sobre-texto">
+          <p class="text-center">
+            A Dra. Aline Gonçalves é graduada em odontologia pela universidade Unicesumar, e especializada em odontopediatria na AMO - Associação Maringaense de Odontologia. Os atendimentos oferecidos no consultório são direcionados tanto para crianças quanto para adultos, sempre com foco em prevenção, cuidado e um ambiente acolhedor para todas as idades, respeitando as necessidades individuais de cada paciente.
+
+          </p>
+          <br>
+          <p class="text-center">
+            Na odontopediatria, os tratamentos têm como foco principal a prevenção da doença cárie, que se inicia desde bebês, passa pela primeira infância, adolescência e chega à fase adulta. Os tratamentos em destaque envolvem orientações de higiene, profilaxia (limpeza), aplicação de flúor, selantes e restaurações. O consultório oferece um ambiente lúdico e acolhedor, criado especialmente para encantar e tranquilizar as crianças.
+
+          </p>
+          <br>
+          <p class="text-center">
+            Já os tratamentos voltados para adultos se destacam por promover saúde bucal por meio de acompanhamento periódico e atendimento humanizado. Isso é especialmente importante para aqueles que sofrem de traumas relacionados ao dentista. Entre os procedimentos mais comuns estão a profilaxia e raspagem supragengival, restaurações estéticas e funcionais, além do clareamento dentário, que proporciona um sorriso mais branco e confiante.
+
+          </p>
+        </div>
+        <div class="sobre-consulta">
+          <p class="text-center">
+            Agende uma consulta, e venha conferir o trabalho dela!
+          </p>
+        </div>
+        </div>
+      </div>
+        </section>
+        <section class="clareamento" id="clareamento">
+          <div class="flex">
+            <div class="container text-center">
+              <div class="row">
+                <div class="col">
+                    <a href="imagens/clareamento1.jpg" data-fslightbox="galeria">
+                      <img src="imagens/clareamento1.jpg" alt="Clareamento">
+                    </a>
+                  </div>
+                </div>
+                <div class="col">
+                  <a href="imagens/clareamento2.jpg" data-fslightbox="galeria">
+                    <img src="imagens/clareamento2.jpg" alt="Clareamento">
+                  </a>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <a href="imagens/clareamento3.jpg" data-fslightbox="galeria">
+                    <img src="imagens/clareamento3.jpg" alt="Clareamento">
+                  </a>
+                </div>
+                <div class="col">
+                  <a href="imagens/clareamento4.jpg" data-fslightbox="galeria">
+                    <img src="imagens/clareamento4.jpg" alt="Clareamento">
+                  </a>
+                </div>
+                <div class="col">
+                  <a href="imagens/clareamento5.jpg" data-fslightbox="galeria">
+                    <img src="imagens/clareamento5.jpg" alt="Clareamento">
+                  </a>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </section>
+        <section class="endereco" id="endereco">
+          <h3 class="text-center"> Localização </h3>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7287.34014333792!2d-52.376371!3d-24.042697000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ed75a5b2621059%3A0xeae8b2990f9f8c18!2sConsult%C3%B3rio%20Odontol%C3%B3gico%20Dra.%20Aline%20Gon%C3%A7alves!5e0!3m2!1spt-BR!2sus!4v1746473881334!5m2!1spt-BR!2sus" width="800" height="300" style="border:0;" 
+          allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="maps"></iframe>
+          <button type="button" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Edifício Centro Comercial Antares - Sala 401 - 4 Andar">
+            <i class="fa-solid fa-caret-down"></i> Mais informações
+          </button>          
+        </section>
+    </main>
+    <footer>
+        </p>
+        <div class="redes-sociais">
+        <a href="https://www.instagram.com/alinegd28?igsh=MTduOHV2cG1qNXNocg==">
+          <i class="fa-brands fa-instagram">
+          </i>
+        </a>
+        <a href="https://www.facebook.com/share/16SygXo4Q2/">
+          <i class="fa-brands fa-facebook"></i>
+        </a>
+      </div>
+      <div class="desenvolvedor">
+      <p> Desenvolvido por:
+      <a href="https://www.linkedin.com/in/beatriz-gomes-santana-0197b5289?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+        Beatriz Gomes Santana
+      </a>
+    </p>
+  </div>
+     
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <!-- const do popOver do bootstrap -->
+<script>
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+</script>
+
+  <!-- script animaçao -->
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+
+<!-- script lightbox -->
+ <script src="https://cdn.jsdelivr.net/npm/fslightbox/index.js" type="module"></script>
+
+</body>
+</html>
