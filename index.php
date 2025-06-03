7e23d73df3a382c03c5faae7cf4a55e61c416ac0
Bia - Integrado <biagsantana1000@gmail.com>
2025-06-02 21:04:19 -0300

    commit php

--- index.php
+++ index.php
@@ -52,118 +52,22 @@
       
           
     </header>
-    <main>
-      <div data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-        <section class="container" id="home">
-          <div class="row row-cols-2">
-            <div class="col">
-              <img src="imagens/Aline-1.jpg" alt="Foto Dr.Aline" class="img-fluid">
-            </div>
-            <div class="col">
-              <h1> Aline </h1>
-              <br>
-              <p>
-                A Dra. Aline Gonçalves possui um atendimento humanizado na Odontologia tanto 
-                em crianças quanto adultos.
-              </p>
-              <p>
-                Venha conhecer o trabalho dela!
-              </p>
-              <div class="contatos">
-              <p>
-                <a href="https://www.instagram.com/alinegd28?igsh=MTduOHV2cG1qNXNocg==">
-                  <i class="fa-brands fa-instagram"></i>
-                  </i>
-                </a>
-                <a href="https://www.facebook.com/share/16SygXo4Q2/">
-                  <i class="fa-brands fa-facebook"></i>
-                  </a>
-                  <a href= "https://wa.link/cn062u">
-                    <i class="fa-brands fa-whatsapp"></i>
-                  </a>
-              </p>
-              </div>
-              </div>
-            </div>
-            </div>
-        </section>
-        <div class="whats">
-          <a href= "https://wa.link/cn062u">
-            <i class="fa-brands fa-whatsapp"></i>
-        </a>
-      </div>
-      <div data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-      <section class="sobre" id="sobre">
-        <h3 class="text-center">Sobre</h3>
-        <div class="sobre-texto">
-          <p class="text-center">
-            A Dra. Aline Gonçalves é graduada em odontologia pela universidade Unicesumar, e especializada em odontopediatria na AMO - Associação Maringaense de Odontologia. Os atendimentos oferecidos no consultório são direcionados tanto para crianças quanto para adultos, sempre com foco em prevenção, cuidado e um ambiente acolhedor para todas as idades, respeitando as necessidades individuais de cada paciente.
+    <main class="container">
+    <?php
+    $pagina = $_GET["param"] ?? "home";
 
-          </p>
-          <br>
-          <p class="text-center">
-            Na odontopediatria, os tratamentos têm como foco principal a prevenção da doença cárie, que se inicia desde bebês, passa pela primeira infância, adolescência e chega à fase adulta. Os tratamentos em destaque envolvem orientações de higiene, profilaxia (limpeza), aplicação de flúor, selantes e restaurações. O consultório oferece um ambiente lúdico e acolhedor, criado especialmente para encantar e tranquilizar as crianças.
+    // $pagina = página que quer abrir
 
-          </p>
-          <br>
-          <p class="text-center">
-            Já os tratamentos voltados para adultos se destacam por promover saúde bucal por meio de acompanhamento periódico e atendimento humanizado. Isso é especialmente importante para aqueles que sofrem de traumas relacionados ao dentista. Entre os procedimentos mais comuns estão a profilaxia e raspagem supragengival, restaurações estéticas e funcionais, além do clareamento dentário, que proporciona um sorriso mais branco e confiante.
+    $pagina = "pages/{$pagina}.php";
 
-          </p>
-        </div>
-        <div class="sobre-consulta">
-          <p class="text-center">
-            Agende uma consulta, e venha conferir o trabalho dela!
-          </p>
-        </div>
-        </div>
-      </div>
-        </section>
-        <section class="clareamento" id="clareamento">
-          <div class="flex">
-            <div class="container text-center">
-              <div class="row">
-                <div class="col">
-                    <a href="imagens/clareamento1.jpg" data-fslightbox="galeria">
-                      <img src="imagens/clareamento1.jpg" alt="Clareamento">
-                    </a>
-                  </div>
-                </div>
-                <div class="col">
-                  <a href="imagens/clareamento2.jpg" data-fslightbox="galeria">
-                    <img src="imagens/clareamento2.jpg" alt="Clareamento">
-                  </a>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <a href="imagens/clareamento3.jpg" data-fslightbox="galeria">
-                    <img src="imagens/clareamento3.jpg" alt="Clareamento">
-                  </a>
-                </div>
-                <div class="col">
-                  <a href="imagens/clareamento4.jpg" data-fslightbox="galeria">
-                    <img src="imagens/clareamento4.jpg" alt="Clareamento">
-                  </a>
-                </div>
-                <div class="col">
-                  <a href="imagens/clareamento5.jpg" data-fslightbox="galeria">
-                    <img src="imagens/clareamento5.jpg" alt="Clareamento">
-                  </a>
-                </div>
-              </div>
-            </div>
-            </div>
-          </div>
-        </section>
-        <section class="endereco" id="endereco">
-          <h3 class="text-center"> Localização </h3>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7287.34014333792!2d-52.376371!3d-24.042697000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ed75a5b2621059%3A0xeae8b2990f9f8c18!2sConsult%C3%B3rio%20Odontol%C3%B3gico%20Dra.%20Aline%20Gon%C3%A7alves!5e0!3m2!1spt-BR!2sus!4v1746473881334!5m2!1spt-BR!2sus" width="800" height="300" style="border:0;" 
-          allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="maps"></iframe>
-          <button type="button" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Edifício Centro Comercial Antares - Sala 401 - 4 Andar">
-            <i class="fa-solid fa-caret-down"></i> Mais informações
-          </button>          
-        </section>
+    //ver se a pagina existe
+
+    if(file_exists($pagina)) {
+      include $pagina;
+    } else {
+      include "pages/erro.php";
+    }
+      ?>
     </main>
     <footer>
         </p>
