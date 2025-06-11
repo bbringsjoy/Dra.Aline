553fc540c1cb484b8b99cf9ca4393ce9fcfccc3a
Bia - Integrado <biagsantana1000@gmail.com>
2025-06-11 19:54:50 -0300

    tentando arrumar a div roxa

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
               <a class="navbar-brand" href="home">
-                <img src="imagens/logocomtexto.png" alt="Logo" class="w-100">
+                <img src="imagens/logocomtexto3.png" alt="Logo" class="w-100">
               </a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
